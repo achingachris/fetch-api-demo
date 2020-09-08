e1587ff417ec5a451c00829871eddd6f5e171f3a
ChrisAchinga <achinga.chris@gmail.com>
2020-09-09 01:06:36 +0300

    home page view

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import Home 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')), # new
+    path('accounts/', include('allauth.urls')), 
+    path('', Home.as_view(), name='home'),
 ]
